d6f87e6f1edd5d7dad13e109b32b01ff0f24b48a
Kim Maida <kim.maida@auth0.com>
2016-10-26 11:13:18 -0400

    Finish simplifying loading component.

--- src/app/core/ui/loading.dir.js
+++ src/app/core/ui/loading.dir.js
@@ -4,41 +4,13 @@
 	angular
 		.module('app')
 		.directive('loading', loading);
-
-	loading.$inject = [];
 	
 	function loading() {
 		// return directive
 		return {
 			restrict: 'EA',
-			templateUrl: 'app/core/ui/loading.tpl.html',
-			scope: {
-				active: '='
-			},
-			controller: loadingCtrl,
-			controllerAs: 'loading',
-			bindToController: true
+			template: '<img class="loading" src="/assets/images/ajax-loader.gif">'
 		};
-	}
-
-	/**
-	 * loading CONTROLLER
-	 * Update the loading status based
-	 * on routeChange state
-	 */
-	function loadingCtrl() {
-		var loading = this;
-
-		_init();
-
-		/**
-		 * INIT function executes procedural code
-		 *
-		 * @private
-		 */
-		function _init() {
-			
-		}
 	}
 
 }());
